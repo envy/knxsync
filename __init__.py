d37b97ef6caca0e6f19f7165566ab7ebfcc04a62
Nico Weichbrodt <nico@weichbrodt.me>
2020-04-28 10:57:15 +0200

    stuff

--- __init__.py
+++ __init__.py
@@ -9,9 +9,20 @@ import logging
 import voluptuous as vol
 from homeassistant.const import ATTR_ENTITY_ID, CONF_ENTITIES, CONF_ENTITY_ID, CONF_ADDRESS, SERVICE_TURN_ON, SERVICE_TURN_OFF, STATE_ON
 from homeassistant.components.light import DOMAIN as DOMAIN_LIGHT, ATTR_RGB_COLOR, ATTR_BRIGHTNESS
+from homeassistant.components.sensor import DOMAIN as DOMAIN_SENSOR
 from homeassistant.components.knx import DOMAIN as DOMAIN_KNX, SERVICE_KNX_SEND, SERVICE_KNX_ATTR_ADDRESS, SERVICE_KNX_ATTR_PAYLOAD 
 from homeassistant.components.knx.light import CONF_STATE_ADDRESS, CONF_BRIGHTNESS_ADDRESS, CONF_BRIGHTNESS_STATE_ADDRESS, CONF_COLOR_ADDRESS, CONF_COLOR_STATE_ADDRESS
 import homeassistant.helpers.config_validation as cv
+
+try:
+    from xknx.knx.dpt_2byte_float import DPT2ByteFloat
+except:
+    from xknx.knx.dpt_float import DPT2ByteFloat
+
+try:
+    from xknx.knx.dpt_2byte_signed import DPT2ByteSigned
+except:
+    from xknx.knx.dpt_2byte import DPT2ByteUnsigned as DPT2ByteSigned
 
 VERSION = '0.0.1'
 
@@ -21,8 +32,6 @@ DATA_KNXSYNC = 'data_knxsync'
 
 DOMAIN = 'knxsync'
 
-CONF_BRIGHTNESS_STEP_ADDRESS = 'brightness_step_address'
-
 CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.Schema({
         vol.Required(CONF_ENTITIES): vol.All(cv.ensure_list, [vol.Schema({
@@ -30,7 +39,6 @@ CONFIG_SCHEMA = vol.Schema({
             vol.Optional(CONF_ADDRESS) : cv.string,
             vol.Optional(CONF_STATE_ADDRESS): cv.string,
             vol.Optional(CONF_BRIGHTNESS_ADDRESS) : cv.string,
-            vol.Optional(CONF_BRIGHTNESS_STEP_ADDRESS) : cv.string,
             vol.Optional(CONF_BRIGHTNESS_STATE_ADDRESS): cv.string,
             vol.Optional(CONF_COLOR_ADDRESS) : cv.string,
             vol.Optional(CONF_COLOR_STATE_ADDRESS): cv.string
@@ -41,7 +49,7 @@ CONFIG_SCHEMA = vol.Schema({
 
 async def async_setup(hass, config):
     """Set up this component."""
-    _LOGGER.info('if you have ANY issues with knxsync, please report them here:'
+    _LOGGER.info('If you have ANY issues with knxsync, please report them here:'
                  ' https://github.com/envy/knxsync')
 
     _LOGGER.debug('KNXSync Version %s', VERSION)
@@ -57,13 +65,19 @@ def get_id(eid: str):
 
 class KNXSyncer:
     def __init__(self, hass, config):
-        self.receivers_onoff = {}
-        self.receivers_brightness = {}
-        self.receivers_brightness_step = {}
-        self.receivers_color = {}
-        self.senders_onoff = {}
-        self.senders_brightness = {}
-        self.senders_color = {}
+        self.entity_map = {
+            DOMAIN_LIGHT: {
+                'receivers_onoff': {},
+                'receivers_brightness': {},
+                'receivers_color': {},
+                'senders_onoff': {},
+                'senders_color': {},
+                'senders_brightness': {}
+            },
+            DOMAIN_SENSOR: {
+                'senders_value': {}
+            }
+        }
         self.hass = hass
 
         entities = config[DOMAIN][CONF_ENTITIES]
@@ -71,36 +85,37 @@ class KNXSyncer:
 
         for entity in entities:
             other_id = entity[CONF_ENTITY_ID]
+            domain = get_domain(other_id)
             _LOGGER.debug("syncing {} to KNX".format(other_id))
 
-            if get_domain(other_id) == 'light':
+            if domain == DOMAIN_LIGHT:
                 if CONF_ADDRESS in entity.keys():
                     _LOGGER.debug("registering receiver {} -> {}".format(entity[CONF_ADDRESS], other_id))
-                    self.receivers_onoff[entity[CONF_ADDRESS]] = other_id
+                    self.entity_map[domain]['receivers_onoff'][entity[CONF_ADDRESS]] = other_id
 
                 if CONF_STATE_ADDRESS in entity.keys():
                     _LOGGER.debug("registering sender {} -> {}".format(other_id, entity[CONF_STATE_ADDRESS]))
-                    self.senders_onoff[other_id] = entity[CONF_STATE_ADDRESS]
+                    self.entity_map[domain]['senders_onoff'][other_id] = entity[CONF_STATE_ADDRESS]
 
                 if CONF_BRIGHTNESS_ADDRESS in entity.keys():
                     _LOGGER.debug("registering receiver {} -> {}".format(entity[CONF_BRIGHTNESS_ADDRESS], other_id))
-                    self.receivers_brightness[entity[CONF_BRIGHTNESS_ADDRESS]] = other_id
-
-                if CONF_BRIGHTNESS_STEP_ADDRESS in entity.keys():
-                    _LOGGER.debug("registering receiver {} -> {}".format(entity[CONF_BRIGHTNESS_ADDRESS], other_id))
-                    self.receivers_brightness_step[entity[CONF_BRIGHTNESS_ADDRESS]] = other_id
+                    self.entity_map[domain]['receivers_brightness'][entity[CONF_BRIGHTNESS_ADDRESS]] = other_id
 
                 if CONF_BRIGHTNESS_STATE_ADDRESS in entity.keys():
                     _LOGGER.debug("registering sender {} -> {}".format(other_id, entity[CONF_BRIGHTNESS_STATE_ADDRESS]))
-                    self.senders_brightness[other_id] = entity[CONF_BRIGHTNESS_STATE_ADDRESS]
+                    self.entity_map[domain]['senders_brightness'][other_id] = entity[CONF_BRIGHTNESS_STATE_ADDRESS]
 
                 if CONF_COLOR_ADDRESS in entity.keys():
                     _LOGGER.debug("registering receiver {} -> {}".format(entity[CONF_COLOR_ADDRESS], other_id))
-                    self.receivers_color[entity[CONF_COLOR_ADDRESS]] = other_id
+                    self.entity_map[domain]['receivers_color'][entity[CONF_COLOR_ADDRESS]] = other_id
 
                 if CONF_COLOR_STATE_ADDRESS in entity.keys():
                     _LOGGER.debug("registering sender {} -> {}".format(other_id, entity[CONF_COLOR_STATE_ADDRESS]))
-                    self.senders_color[other_id] = entity[CONF_COLOR_STATE_ADDRESS]
+                    self.entity_map[domain]['senders_color'][other_id] = entity[CONF_COLOR_STATE_ADDRESS]
+            elif domain == DOMAIN_SENSOR:
+                if CONF_STATE_ADDRESS in entity.keys():
+                    _LOGGER.debug("registering sender {} -> {}".format(other_id, entity[CONF_STATE_ADDRESS]))
+                    self.entity_map[domain]['senders_value'][other_id] = entity[CONF_STATE_ADDRESS]
 
         hass.bus.async_listen('knx_event', self.got_telegram)
         hass.bus.async_listen('state_changed', self.state_changed)
@@ -111,83 +126,78 @@ class KNXSyncer:
         domain = get_domain(other_id)
         if 'new_state' not in data.keys():
             return
-
         state = data['new_state']
-        if other_id in self.senders_onoff.keys():
-            address = self.senders_onoff[other_id]
-            if domain == DOMAIN_LIGHT:
+
+        if domain == DOMAIN_LIGHT:
+            if other_id in self.entity_map[domain]['senders_onoff'].keys():
+                address = self.entity_map[domain]['senders_onoff'][other_id]
                 if state.state == STATE_ON:
                     _LOGGER.debug("Sending {} on -> {}".format(other_id, address))
-                    payload = [1]
+                    payload = 1
                 else:
                     _LOGGER.debug("Sending {} off -> {}".format(other_id, address))
-                    payload = [0]
+                    payload = 0
                 await self.hass.services.async_call(DOMAIN_KNX, SERVICE_KNX_SEND, { SERVICE_KNX_ATTR_ADDRESS: address, SERVICE_KNX_ATTR_PAYLOAD: payload})
 
-        if other_id in self.senders_color.keys() and ATTR_RGB_COLOR in state.attributes.keys():
-            address = self.senders_color[other_id]
-            rgb = state.attributes[ATTR_RGB_COLOR]
-            if domain == DOMAIN_LIGHT:
+            if other_id in self.entity_map[domain]['senders_color'].keys() and ATTR_RGB_COLOR in state.attributes.keys():
+                address = self.entity_map[domain]['senders_color'][other_id]
+                rgb = state.attributes[ATTR_RGB_COLOR]
                 payload = list(rgb)
                 _LOGGER.debug("Sending {} color -> {}".format(other_id, address))
                 await self.hass.services.async_call(DOMAIN_KNX, SERVICE_KNX_SEND, { SERVICE_KNX_ATTR_ADDRESS: address, SERVICE_KNX_ATTR_PAYLOAD: payload})
 
-        if other_id in self.senders_brightness.keys() and ATTR_BRIGHTNESS in state.attributes.keys():
-            address = self.senders_brightness[other_id]
-            brightness = state.attributes[ATTR_BRIGHTNESS]
-            if domain == DOMAIN_LIGHT:
+            if other_id in self.entity_map[domain]['senders_brightness'].keys() and ATTR_BRIGHTNESS in state.attributes.keys():
+                address = self.entity_map[domain]['senders_brightness'][other_id]
+                brightness = state.attributes[ATTR_BRIGHTNESS]
                 payload = [brightness] # XKNX requires a list for 1 byte payload
                 _LOGGER.debug("Sending {} brightness -> {}".format(other_id, address))
                 await self.hass.services.async_call(DOMAIN_KNX, SERVICE_KNX_SEND, { SERVICE_KNX_ATTR_ADDRESS: address, SERVICE_KNX_ATTR_PAYLOAD: payload})
+        elif domain == DOMAIN_SENSOR:
+            if other_id in self.entity_map[domain]['senders_value'].keys():
+                address = self.entity_map[domain]['senders_value'][other_id]
+                value = state.state
+                # TODO: maybe the datatype should be given in the config...
+                try:
+                    value = int(value)
+                    payload = list(DPT2ByteSigned.to_knx(value))
+                    await self.hass.services.async_call(DOMAIN_KNX, SERVICE_KNX_SEND, { SERVICE_KNX_ATTR_ADDRESS: address, SERVICE_KNX_ATTR_PAYLOAD: payload})
+                except ValueError:
+                    try:
+                        value = float(value)
+                        payload = list(DPT2ByteFloat.to_knx(value))
+                        await self.hass.services.async_call(DOMAIN_KNX, SERVICE_KNX_SEND, { SERVICE_KNX_ATTR_ADDRESS: address, SERVICE_KNX_ATTR_PAYLOAD: payload})
+                    except ValueError:
+                        # Value is neither int nor float
+                        return
+
 
     async def got_telegram(self, event):
         data = event.data
         address = data['address']
-        if address in self.receivers_onoff.keys():
-            other_id = self.receivers_onoff[address]
+        if address in self.entity_map[DOMAIN_LIGHT]['receivers_onoff'].keys():
+            other_id = self.entity_map[DOMAIN_LIGHT]['receivers_onoff'][address]
             payload = data['data']
             _LOGGER.debug("got data {} for {}".format(payload, other_id))
-            if get_domain(other_id) == DOMAIN_LIGHT:
-                if payload == 1:
-                    _LOGGER.debug("Turning {} on <- {}".format(other_id, address))
-                    await self.hass.services.async_call(DOMAIN_LIGHT, SERVICE_TURN_ON, { ATTR_ENTITY_ID: other_id })
-                elif payload == 0:
-                    _LOGGER.debug("Turning {} off <- {}".format(other_id, address))
-                    await self.hass.services.async_call(DOMAIN_LIGHT, SERVICE_TURN_OFF, { ATTR_ENTITY_ID: other_id })
+            if payload == 1:
+                _LOGGER.debug("Turning {} on <- {}".format(other_id, address))
+                await self.hass.services.async_call(DOMAIN_LIGHT, SERVICE_TURN_ON, { ATTR_ENTITY_ID: other_id })
+            elif payload == 0:
+                _LOGGER.debug("Turning {} off <- {}".format(other_id, address))
+                await self.hass.services.async_call(DOMAIN_LIGHT, SERVICE_TURN_OFF, { ATTR_ENTITY_ID: other_id })
 
-        if address in self.receivers_color.keys():
-            other_id = self.receivers_color[address]
+        if address in self.entity_map[DOMAIN_LIGHT]['receivers_color'].keys():
+            other_id = self.entity_map[DOMAIN_LIGHT]['receivers_color'][address]
             payload = data['data']
-            if get_domain(other_id) == DOMAIN_LIGHT:
-                if len(payload) == 3:
-                    _LOGGER.debug("Turning {} on with color <- {}".format(other_id, address))
-                    await self.hass.services.async_call(DOMAIN_LIGHT, SERVICE_TURN_ON, { ATTR_ENTITY_ID: other_id, ATTR_RGB_COLOR: payload })
+            if len(payload) == 3:
+                _LOGGER.debug("Turning {} on with color <- {}".format(other_id, address))
+                await self.hass.services.async_call(DOMAIN_LIGHT, SERVICE_TURN_ON, { ATTR_ENTITY_ID: other_id, ATTR_RGB_COLOR: payload })
 
-        if address in self.receivers_brightness.keys():
-            other_id = self.receivers_brightness[address]
+        if address in self.entity_map[DOMAIN_LIGHT]['receivers_brightness'].keys():
+            other_id = self.entity_map[DOMAIN_LIGHT]['receivers_brightness'][address]
             payload = data['data']
-            if get_domain(other_id) == DOMAIN_LIGHT:
-                if payload[0] == 0:
-                    _LOGGER.debug("Turning {} off with brightness <- {}".format(other_id, address))
-                    await self.hass.services.async_call(DOMAIN_LIGHT, SERVICE_TURN_OFF, { ATTR_ENTITY_ID: other_id })
-                else:
-                    _LOGGER.debug("Turning {} on with brightness <- {}".format(other_id, address))
-                    await self.hass.services.async_call(DOMAIN_LIGHT, SERVICE_TURN_ON, { ATTR_ENTITY_ID: other_id, ATTR_BRIGHTNESS: payload[0] })
-
-        if address in self.receivers_brightness_step.keys():
-            other_id = self.receivers_brightness_step[address]
-            payload = data['data']
-            if get_domain(other_id) == DOMAIN_LIGHT:
-                if payload == 0:
-                    # we need to stop
-                    pass
-                else:
-                    sign = payload & 0b1000
-                    # we assume the stepwidth is going to be 100%
-                    if sign > 0:
-                        # we need to increase brightness
-                        pass
-                    else:
-                        # we need to decrease brightness
-                        pass
-                
+            if payload[0] == 0:
+                _LOGGER.debug("Turning {} off with brightness <- {}".format(other_id, address))
+                await self.hass.services.async_call(DOMAIN_LIGHT, SERVICE_TURN_OFF, { ATTR_ENTITY_ID: other_id })
+            else:
+                _LOGGER.debug("Turning {} on with brightness <- {}".format(other_id, address))
+                await self.hass.services.async_call(DOMAIN_LIGHT, SERVICE_TURN_ON, { ATTR_ENTITY_ID: other_id, ATTR_BRIGHTNESS: payload[0] })
