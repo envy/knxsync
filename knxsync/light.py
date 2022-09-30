3a24509c156cd2e66b28f6bebabe65695ba9ac42
Nico Weichbrodt <git@nico.weichbrodt.me>
2022-09-30 19:57:01 +0200

    make is possible to use GA lists

--- knxsync/light.py
+++ knxsync/light.py
@@ -31,29 +31,29 @@ _LOGGER = logging.getLogger(DOMAIN)
 class SyncedLight(SyncedEntity):
     def __init__(self, hass: HomeAssistant, synced_entity_id: str, entity_config: dict):
         super().__init__(hass, synced_entity_id, entity_config)
-        self.address: str | None = None
-        self.state_address: str | None = None
-        self.brightness_address: str | None = None
-        self.brightness_state_address: str | None = None
+        self.address: [str] | None = None
+        self.state_address: [str] | None = None
+        self.brightness_address: [str] | None = None
+        self.brightness_state_address: [str] | None = None
         self.zero_brightness_when_off: bool | None = None
-        self.color_address: str | None = None
-        self.color_state_address: str | None = None
+        self.color_address: [str] | None = None
+        self.color_state_address: [str] | None = None
         _LOGGER.debug(f"Setting up synced light '{self.synced_entity_id}'")
 
         if CONF_ADDRESS in entity_config.keys():
-            self.address = entity_config[CONF_ADDRESS]
+            self.address = list(map(lambda x: x.strip(), entity_config[CONF_ADDRESS].split(",")))
             _LOGGER.debug(f"{self.synced_entity_id} <- {self.address}")
 
         if CONF_STATE_ADDRESS in entity_config.keys():
-            self.state_address = entity_config[CONF_STATE_ADDRESS]
+            self.state_address = list(map(lambda x: x.strip(), entity_config[CONF_STATE_ADDRESS].split(",")))
             _LOGGER.debug(f"{self.synced_entity_id} -> {self.state_address}")
 
         if LightSchema.CONF_BRIGHTNESS_ADDRESS in entity_config.keys():
-            self.brightness_address = entity_config[LightSchema.CONF_BRIGHTNESS_ADDRESS]
+            self.brightness_address = list(map(lambda x: x.strip(), entity_config[LightSchema.CONF_BRIGHTNESS_ADDRESS].split(",")))
             _LOGGER.debug(f"{self.synced_entity_id} <- {self.brightness_address}")
 
         if LightSchema.CONF_BRIGHTNESS_STATE_ADDRESS in entity_config.keys():
-            self.brightness_state_address = entity_config[LightSchema.CONF_BRIGHTNESS_STATE_ADDRESS]
+            self.brightness_state_address = list(map(lambda x: x.strip(), entity_config[LightSchema.CONF_BRIGHTNESS_STATE_ADDRESS].split(",")))
             _LOGGER.debug(f"{self.synced_entity_id} -> {self.brightness_state_address}")
 
         if CONF_KNXSYNC_LIGHT_ZERO_BRIGHTNESS_WHEN_OFF in entity_config.keys():
@@ -61,11 +61,11 @@ class SyncedLight(SyncedEntity):
             _LOGGER.debug(f"{self.synced_entity_id} will also report off as 0% brightness.")
 
         if LightSchema.CONF_COLOR_ADDRESS in entity_config.keys():
-            self.color_address = entity_config[LightSchema.CONF_COLOR_ADDRESS]
+            self.color_address = list(map(lambda x: x.strip(), entity_config[LightSchema.CONF_COLOR_ADDRESS].split(",")))
             _LOGGER.debug(f"{self.synced_entity_id} <- {self.color_address}")
 
         if LightSchema.CONF_COLOR_STATE_ADDRESS in entity_config.keys():
-            self.color_state_address = entity_config[LightSchema.CONF_COLOR_STATE_ADDRESS]
+            self.color_state_address = list(map(lambda x: x.strip(), entity_config[LightSchema.CONF_COLOR_STATE_ADDRESS].split(",")))
             _LOGGER.debug(f"{self.synced_entity_id} -> {self.color_state_address}")
 
     async def async_got_telegram(self, event: Event) -> None:
@@ -74,28 +74,28 @@ class SyncedLight(SyncedEntity):
         type = data['telegramtype']
 
         if type == TELEGRAMTYPE_WRITE:
-            if address == self.address:
+            if address in self.address:
                 payload = data['data']
                 if payload == 1:
-                    _LOGGER.debug(f"Turning {self.synced_entity_id} on <- {self.address}")
+                    _LOGGER.debug(f"Turning {self.synced_entity_id} on <- {address}")
                     await self.hass.services.async_call(DOMAIN_LIGHT, SERVICE_TURN_ON, { ATTR_ENTITY_ID: self.synced_entity_id })
                 elif payload == 0:
-                    _LOGGER.debug(f"Turning {self.synced_entity_id} off <- {self.address}")
+                    _LOGGER.debug(f"Turning {self.synced_entity_id} off <- {address}")
                     await self.hass.services.async_call(DOMAIN_LIGHT, SERVICE_TURN_OFF, { ATTR_ENTITY_ID: self.synced_entity_id })
 
-            if address == self.brightness_address:
+            if address in self.brightness_address:
                 payload = data['data']
                 if payload[0] == 0:
-                    _LOGGER.debug(f"Turning {self.synced_entity_id} off with brightness <- {self.brightness_address}")
+                    _LOGGER.debug(f"Turning {self.synced_entity_id} off with brightness <- {address}")
                     await self.hass.services.async_call(DOMAIN_LIGHT, SERVICE_TURN_OFF, { ATTR_ENTITY_ID: self.synced_entity_id })
                 else:
-                    _LOGGER.debug(f"Turning {self.synced_entity_id} on with brightness <- {self.brightness_address}")
+                    _LOGGER.debug(f"Turning {self.synced_entity_id} on with brightness <- {address}")
                     await self.hass.services.async_call(DOMAIN_LIGHT, SERVICE_TURN_ON, { ATTR_ENTITY_ID: self.synced_entity_id, ATTR_BRIGHTNESS: payload[0] })
 
-            if address == self.color_address:
+            if address in self.color_address:
                 payload = data['data']
                 if len(payload) == 3:
-                    _LOGGER.debug(f"Turning {self.synced_entity_id} on with color <- {self.color_address}")
+                    _LOGGER.debug(f"Turning {self.synced_entity_id} on with color <- {address}")
                     await self.hass.services.async_call(DOMAIN_LIGHT, SERVICE_TURN_ON, { ATTR_ENTITY_ID: self.synced_entity_id, ATTR_RGB_COLOR: payload })
         elif type == TELEGRAMTYPE_READ and self.answer_reads and self.state is not None:
             _LOGGER.debug(f"Reading state for {self.synced_entity_id} <- {address}")
@@ -122,29 +122,35 @@ class SyncedLight(SyncedEntity):
 
     async def async_setup_events(self) -> None:
         if self.address is not None:
-            _LOGGER.debug(f"registering receiver {self.address} -> {self.synced_entity_id}")
-            await self.hass.services.async_call(DOMAIN_KNX, SERVICE_KNX_EVENT_REGISTER, { KNX_ADDRESS: self.address })
+            for address in self.address:
+                _LOGGER.debug(f"registering receiver {address} -> {self.synced_entity_id}")
+                await self.hass.services.async_call(DOMAIN_KNX, SERVICE_KNX_EVENT_REGISTER, { KNX_ADDRESS: address })
 
         if self.brightness_address is not None:
-            _LOGGER.debug(f"registering receiver {self.brightness_address} -> {self.synced_entity_id}")
-            await self.hass.services.async_call(DOMAIN_KNX, SERVICE_KNX_EVENT_REGISTER, { KNX_ADDRESS: self.brightness_address })
+            for address in self.brightness_address:
+                _LOGGER.debug(f"registering receiver {address} -> {self.synced_entity_id}")
+                await self.hass.services.async_call(DOMAIN_KNX, SERVICE_KNX_EVENT_REGISTER, { KNX_ADDRESS: address })
 
         if self.color_address is not None:
-            _LOGGER.debug(f"registering receiver {self.color_address} -> {self.synced_entity_id}")
-            await self.hass.services.async_call(DOMAIN_KNX, SERVICE_KNX_EVENT_REGISTER, { KNX_ADDRESS: self.color_address })
+            for address in self.color_address:
+                _LOGGER.debug(f"registering receiver {address} -> {self.synced_entity_id}")
+                await self.hass.services.async_call(DOMAIN_KNX, SERVICE_KNX_EVENT_REGISTER, { KNX_ADDRESS: address })
 
         if not self.answer_reads:
             return
         # Register for potential reads
         if self.state_address is not None:
-            _LOGGER.debug(f"registering receiver {self.state_address} <- {self.synced_entity_id}")
-            await self.hass.services.async_call(DOMAIN_KNX, SERVICE_KNX_EVENT_REGISTER, { KNX_ADDRESS: self.state_address })
+            for address in self.state_address:
+                _LOGGER.debug(f"registering receiver {address} <- {self.synced_entity_id}")
+                await self.hass.services.async_call(DOMAIN_KNX, SERVICE_KNX_EVENT_REGISTER, { KNX_ADDRESS: address })
         if self.brightness_state_address is not None:
-            _LOGGER.debug(f"registering receiver {self.brightness_state_address} <- {self.synced_entity_id}")
-            await self.hass.services.async_call(DOMAIN_KNX, SERVICE_KNX_EVENT_REGISTER, { KNX_ADDRESS: self.brightness_state_address })
+            for address in self.brightness_state_address:
+                _LOGGER.debug(f"registering receiver {address} <- {self.synced_entity_id}")
+                await self.hass.services.async_call(DOMAIN_KNX, SERVICE_KNX_EVENT_REGISTER, { KNX_ADDRESS: address })
         if self.color_state_address is not None:
-            _LOGGER.debug(f"registering receiver {self.color_state_address} <- {self.synced_entity_id}")
-            await self.hass.services.async_call(DOMAIN_KNX, SERVICE_KNX_EVENT_REGISTER, { KNX_ADDRESS: self.color_state_address })
+            for address in self.color_state_address:
+                _LOGGER.debug(f"registering receiver {address} <- {self.synced_entity_id}")
+                await self.hass.services.async_call(DOMAIN_KNX, SERVICE_KNX_EVENT_REGISTER, { KNX_ADDRESS: address })
 
     async def _send_onoff(self, response: bool = False) -> None:
         if self.state == None:
@@ -155,10 +161,12 @@ class SyncedLight(SyncedEntity):
         else:
             _LOGGER.debug(f"Sending {self.synced_entity_id} off -> {self.state_address}")
             payload = 0
-        await self.hass.services.async_call(DOMAIN_KNX, SERVICE_KNX_SEND, { KNX_ADDRESS: self.state_address, SERVICE_KNX_ATTR_PAYLOAD: payload, SERVICE_KNX_ATTR_RESPONSE: response})
+        for address in self.state_address:
+            await self.hass.services.async_call(DOMAIN_KNX, SERVICE_KNX_SEND, { KNX_ADDRESS: address, SERVICE_KNX_ATTR_PAYLOAD: payload, SERVICE_KNX_ATTR_RESPONSE: response})
         if not response and self.brightness_state_address is not None and self.zero_brightness_when_off and payload == 0:
             payload = [0]
-            await self.hass.services.async_call(DOMAIN_KNX, SERVICE_KNX_SEND, { KNX_ADDRESS: self.brightness_state_address, SERVICE_KNX_ATTR_PAYLOAD: payload})
+            for address in self.brightness_state_address:
+                await self.hass.services.async_call(DOMAIN_KNX, SERVICE_KNX_SEND, { KNX_ADDRESS: address, SERVICE_KNX_ATTR_PAYLOAD: payload})
 
     async def _send_brightness(self, response: bool = False) -> None:
         if self.state == None:
@@ -166,7 +174,8 @@ class SyncedLight(SyncedEntity):
         brightness = self.state.attributes[ATTR_BRIGHTNESS]
         payload = [brightness] # XKNX requires a list for 1 byte payload
         _LOGGER.debug(f"Sending {self.synced_entity_id} brightness -> {self.brightness_state_address}")
-        await self.hass.services.async_call(DOMAIN_KNX, SERVICE_KNX_SEND, { KNX_ADDRESS: self.brightness_state_address, SERVICE_KNX_ATTR_PAYLOAD: payload, SERVICE_KNX_ATTR_RESPONSE: response})
+        for address in self.brightness_state_address:
+            await self.hass.services.async_call(DOMAIN_KNX, SERVICE_KNX_SEND, { KNX_ADDRESS: address, SERVICE_KNX_ATTR_PAYLOAD: payload, SERVICE_KNX_ATTR_RESPONSE: response})
 
     async def _send_color(self, reponse: bool = False) -> None:
         if self.state == None:
@@ -174,4 +183,5 @@ class SyncedLight(SyncedEntity):
         rgb = self.state.attributes[ATTR_RGB_COLOR]
         payload = list(rgb)
         _LOGGER.debug(f"Sending {self.synced_entity_id} color -> {self.color_state_address}")
-        await self.hass.services.async_call(DOMAIN_KNX, SERVICE_KNX_SEND, { KNX_ADDRESS: self.color_state_address, SERVICE_KNX_ATTR_PAYLOAD: payload, SERVICE_KNX_ATTR_RESPONSE: reponse})
+        for address in self.color_state_address:
+            await self.hass.services.async_call(DOMAIN_KNX, SERVICE_KNX_SEND, { KNX_ADDRESS: address, SERVICE_KNX_ATTR_PAYLOAD: payload, SERVICE_KNX_ATTR_RESPONSE: reponse})
